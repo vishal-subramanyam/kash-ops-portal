b0303da24ef036a7f14a8c0929b8d91a4aa7b1b9
Garrett <garrettleegrahamanderson@gmail.com>
2024-04-03 18:45:59 +0000

    working on the pie chart display regarding the legend and pie chart

--- src/components/PieChartKPI.js
+++ src/components/PieChartKPI.js
@@ -8,20 +8,25 @@ function PieChartKPI(props) {
       <h4>{props.projectArr[0].projectName}</h4>
       <PieChart
         //   colors={["red", "blue", "green"]} // Use palette
+        margin={{ top: 0, bottom: 0, left: 100, right: 100 }}
         series={[
           {
             data: [...props.projectArr],
-            innerRadius: 27,
-            cx: 150,
+            innerRadius: 20,
+            // cx: 100,
           },
         ]}
-        width={props.projectArr.length <= 5 ? 450 : 600}
+        // width={props.projectArr.length <= 5 ? 450 : 600}
         height={200}
         slotProps={{
           legend: {
             // hidden: true,
-            direction: "column",
-            position: { vertical: "bottom", horizontal: "right" },
+            direction: "row",
+            position: { vertical: "bottom", horizontal: "middle" },
+            padding: { top: 0, bottom: 0, left: 0, right: 0 },
+            labelStyle: {
+              fontSize: 12,
+            },
           },
         }}
       />
