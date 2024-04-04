5d892a6a5d0ed1ff90969086f6383a99f34cf86d
Garrett <garrettleegrahamanderson@gmail.com>
2024-04-04 15:54:31 +0000

    working on getting the burn rate chart working. manually updated the mui x-charts version to v7 and updated the mui material package to 5.15.14 in both package.json and package-lock.json

--- src/components/BurnRateChartKPI.js
+++ src/components/BurnRateChartKPI.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Gauge } from "@mui/x-charts/Gauge";
+import { Gauge } from "@mui/x-charts";
+// import { Gauge } from "@mui/x-charts/Gauge";
 import "../assets/styles/ControlCenter.css";
 
 function BurnRateChartKPI(props) {
   return (
     <div className="BurnRateChart--container">
       <h4>Project Title</h4>
-      <Gauge width={100} height={100} value={50} valueMin={10} valueMax={60} />
+      {/* <Gauge width={100} height={100} value={50} valueMin={10} valueMax={60} /> */}
     </div>
   );
 }
